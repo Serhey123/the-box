ecd0b0037b01908622b665f6aaaea53cdd92cd6f
Your Name <hojolkitybo@gmail.com>
2022-12-11 21:31:27 -0600

    created navigation and slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import 'normalize.css';
 import './scss/main.scss';
+
+import Swiper, { Navigation } from 'swiper';
+
+import 'swiper/swiper.min.css';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+});
